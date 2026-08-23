5c586cfa6d885501cf8b39c45dfde0bf9a473e2b
Brian Kuzmanoski <bkuzmanoski@icloud.com>
2026-08-23 20:17:23 +1000

    Tweak

--- src/views/content-body.tsx
+++ src/views/content-body.tsx
@@ -6,8 +6,7 @@ import { mdxComponents } from "#/content/mdx-components";
 import type { MDXContent } from "mdx/types";
 
 export function ContentBody({ content }: { content: Promise<{ default: MDXContent }> }) {
-  // Read the module with `use()` rather than the route loader as loader data must be serializable.
-  const { default: MDXContent } = use(content);
+  const { default: MDXContent } = use(content); // Read the module with `use()` rather than the route loader as loader data must be serializable.
 
   return (
     <MDXProvider components={mdxComponents}>
